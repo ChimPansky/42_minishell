d84e95600606dbad7983fa86b2771728612e41dc
Thomas Kasbarian2 <thomas.kasbarian@gmail.com>
2024-02-08 17:43:56 +0100

    pwd ignores parameters

--- src/built_ins/pwd.c
+++ src/built_ins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: tkasbari <thomas.kasbarian@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 17:33:15 by tkasbari          #+#    #+#             */
-/*   Updated: 2023/12/28 20:02:34 by tkasbari         ###   ########.fr       */
+/*   Updated: 2024/02/08 17:31:06 by tkasbari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,14 @@
 
 int	built_in_pwd(t_msh *msh, char **cmd_with_args, int fd_out)
 {
-	(void)cmd_with_args;
 	(void)msh;
+	(void)cmd_with_args;
 	char *cwd;
 
-	if (cmd_with_args[1] != NULL)
-		return (ft_printf_err("pwd: too many arguments\n"), EXIT_FAILURE);
 	cwd = getcwd(NULL, 0);
 	if (!cwd)
 		return (perror("pwd"), !EXIT_FAILURE);
-	dprintf(fd_out, "%s\n", cwd);
+	ft_dprintf(fd_out, "%s\n", cwd);
 	free(cwd);
 	return (EXIT_SUCCESS);
 }
