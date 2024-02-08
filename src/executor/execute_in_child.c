a9126478af0fc9ec7bb4d5f9a019c52e32765df4
Vladimir Vilenskii <vvilensk@c1r2p10.42vienna.com>
2024-02-08 19:54:16 +0100

    fix

--- src/executor/execute_in_child.c
+++ src/executor/execute_in_child.c
@@ -6,7 +6,7 @@
 /*   By: vvilensk <vilenskii.v@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 19:48:14 by vvilensk          #+#    #+#             */
-/*   Updated: 2024/02/08 19:48:15 by vvilensk         ###   ########.fr       */
+/*   Updated: 2024/02/08 19:53:26 by vvilensk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,24 +25,27 @@ static int	try_exec_built_in(t_msh *msh, char **cmd_with_args)
 	return (SUCCESS);
 }
 
-static int	check_permissions(t_msh *msh, const char *exec)
+static int	check_permissions(
+	t_msh *msh,
+	const char *exec_in_path,
+	const char *exec)
 {
 	struct stat	fstat;
 
-	if (exec == NULL || SUCCESS != access(exec, F_OK))
+	if (exec_in_path == NULL || SUCCESS != access(exec_in_path, F_OK))
 		msh->last_exit_code = EXIT_COMMAND_NOT_FOUND;
-	if (exec == NULL)
+	if (exec_in_path == NULL)
 		return (ft_printf_err("msh: command not found: %s\n", exec), !SUCCESS);
-	if (SUCCESS != access(exec, F_OK))
+	if (SUCCESS != access(exec_in_path, F_OK))
 		return (ft_printf_err("msh: %s: no such file or directory\n", exec),
 			!SUCCESS);
-	if (SUCCESS != stat(exec, &fstat))
+	if (SUCCESS != stat(exec_in_path, &fstat))
 		return (perror("check_permissions: stat"), !SUCCESS);
-	if (!S_ISDIR(fstat.st_mode) || SUCCESS != access(exec, X_OK))
+	if (S_ISDIR(fstat.st_mode) || SUCCESS != access(exec_in_path, X_OK))
 		msh->last_exit_code = EXIT_PERMISSION_DENIED;
-	if (!S_ISDIR(fstat.st_mode))
+	if (S_ISDIR(fstat.st_mode))
 		return (ft_printf_err("msh: %s: is a directory\n", exec), !SUCCESS);
-	if (SUCCESS != access(exec, X_OK))
+	if (SUCCESS != access(exec_in_path, X_OK))
 		return (ft_printf_err("msh: %s: permission denied\n", exec), !SUCCESS);
 	return (SUCCESS);
 }
@@ -59,7 +62,7 @@ int	execute_in_child_process(t_msh *msh, char **cmd_with_args)
 		return (SUCCESS);
 	if (SUCCESS != init_path_finder(msh, &pf, cmd_with_args[0]))
 		return (!SUCCESS);
-	if (check_permissions(msh, pf.exec) != SUCCESS)
+	if (check_permissions(msh, pf.exec_with_path, pf.exec) != SUCCESS)
 		return (destroy_path_finder(&pf), SUCCESS);
 	if (varlist_convert_to_array(msh->env, &envp) != SUCCESS)
 		return (destroy_path_finder(&pf), perror("execute_in_child_process: "
